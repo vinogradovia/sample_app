4ef165f68125121bfa32b0710f5638d3a2bd6556
vinogradovia <vinogradovia@gmail.com>
2013-05-13 00:02:54 +0400

    Make a basic User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 #return the full title on a per-page basic
 	def full_title(page_title)
-		base_title = 'Ruby on Rais Tutorial Sample App'
+		base_title = 'Ruby on Rails Tutorial Sample App'
 		if page_title.empty?
 			base_title
 		else
